c04194d538201e974836fdcd4dd133bf7bcf2652
mr-nuralam <muhammadrisqinuralam@gmail.com>
2022-10-17 00:01:04 +0700

    Challenge Tabel and ToDo

---
+++ To Do App/script.js
@@ -0,0 +1,21 @@
+
+const onSubmit = (event) => {
+
+    event.preventDefault();
+
+    const {name} = event.target;
+
+    list.innerHTML +=
+        `<li> 
+        ${name.value} 
+        <button type="button" class="btn btn-danger m-2 fs-5 float-end" onclick="removeItem(event)">delete</button>
+        </li>`;
+}
+
+addEventListener("submit",onSubmit);
+
+function removeItem(event) {
+    event.preventDefault();
+    let li = event.target.parentNode;
+    list.removeChild(li);
+}
